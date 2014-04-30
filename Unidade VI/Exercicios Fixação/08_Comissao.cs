7d76c55fe262c82402c81bf4dc0705d484898cf1
Lucas Muniz <lucas_goedert@hotmail.com>
2014-04-30 08:59:12 -0300

    Exercicio 8 acrescentado

---
+++ Unidade VI/Exercicios Fixação/08_Comissao.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unidade_VI.Exercicios_Fixação
+{
+    
+    
+    class _08_Comissao
+    {
+        /*8) Uma empresa de vendas tem três corretores. A empresa paga ao corretor uma 
+        comissão calculada de acordo com o valor de suas vendas. Se o valor da venda 
+        de um corretor for maior que R$ 50.000.00 a comissão será de 12% do valor 
+        vendido. Se o valor da venda do corretor estiver entre R$ 30.000.00 e R$ 
+        50.000.00 (incluindo extremos) a comissão será de 9.5%. Em qualquer outro 
+        caso, a comissão será de 7%. Escreva um algoritmo que gere um relatório 
+        contendo nome, valor da venda e comissão de cada um dos corretores. O 
+        relatório deve mostrar também o total de vendas da empresa. 
+         */
+        public static double valorVenda, comissao;
+        public static string nome;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o seu nome: ");
+            nome = Console.ReadLine();
+            Console.WriteLine("Digite o total de vendas do mes: ");
+            valorVenda = Convert.ToDouble(Console.ReadLine());
+
+            if (valorVenda>50000)
+            {
+                comissao = valorVenda * 0.12;
+                
+            }
+            if (valorVenda>=30000 && valorVenda<=50000)
+            {
+                comissao = valorVenda * 0.095;
+            }
+            else
+            {
+                comissao = valorVenda * 0.07;
+            }
+            
+            Console.WriteLine("========= RESULTADO ========\n");
+            Console.WriteLine("O nome do vendedor é: {0}",nome);
+            Console.WriteLine("O valor das vendas é: {0}",valorVenda);
+            Console.WriteLine("O valor da comissao é: {0}",comissao);
+            Console.ReadKey();
+        }
+
+
+    }
+}
