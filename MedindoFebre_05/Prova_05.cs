d36b69b55a43c1e5741decebc833b68e013435bb
Lucas Muniz <lucas_goedert@hotmail.com>
2014-05-07 11:12:03 -0300

    Medindo a febre

---
+++ MedindoFebre_05/Prova_05.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedindoFebre_05
+{
+    class Prova_05
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
