586c426a7b3247b4666a5d15c65506390641edcc
lucasmuniz <lucas_goedert@hotmail.com>
2014-05-07 16:45:18 -0300

    Exercicio 03 concluido

--- Unidade VII/Exercicios Fixação/02_Prefeitura.cs
+++ Unidade VII/Exercicios Fixação/02_Prefeitura.cs
@@ -12,10 +12,10 @@ namespace Unidade_VII.Exercicios_Fixação
         public static double salarioHabitante,mediaSalarial,mediaDeFilhos,maiorSalario=0,porcentagem_pobres;
         public static double valorSalarioTotal;
 
-        public static void Main(string[] args)
+        public static void Main02(string[] args)
         {
             /* A prefeitura de uma cidade deseja fazer uma pesquisa entre seus habitantes. Faça um algoritmos para 
-             * coletar dados sobre o salário e número de filhos de cada habitante e após as leituras, escrever:           
+               coletar dados sobre o salário e número de filhos de cada habitante e após as leituras, escrever:           
             a) Média de salário da população 
             b) Média do número de filhos 
             c) Maior salário dos habitantes 
@@ -39,24 +39,18 @@ namespace Unidade_VII.Exercicios_Fixação
                 if (salarioHabitante>maiorSalario)
                 {
                     maiorSalario = salarioHabitante;
-                }
-                
+                }            
                 valorSalarioTotal = valorSalarioTotal + salarioHabitante;
-
                 Console.WriteLine("Digite o total de filhos: ");
                 filhosPorFamilia = Convert.ToInt16(Console.ReadLine());
                 totalDeFilhos = totalDeFilhos + filhosPorFamilia;
-                
-          
             }
 
             ResultadoFinal();
             program = 0;
         }
-
         private static void ResultadoFinal()
-        {
-            
+        {      
             mediaSalarial = valorSalarioTotal / contadorDeHabit;
             mediaDeFilhos = totalDeFilhos / contadorDeHabit;         
             porcentagem_pobres =(contadorDeHabit/contadorMenor_CentoECinquenta)*10;
@@ -65,7 +59,6 @@ namespace Unidade_VII.Exercicios_Fixação
             Console.WriteLine("A média de filhos por habitante na cidade e : {0}",mediaDeFilhos);
             Console.WriteLine("O maior salário da população é: {0}R$",maiorSalario);
             Console.WriteLine("A porcentagem da população que possui baixa renda é {0}%", porcentagem_pobres.ToString("##.##"));
-
             Console.ReadKey();
         }
         
---
+++ Unidade VII/Exercicios Fixação/03_CadastroLoja.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unidade_VII.Exercicios_Fixação
+{
+    class _03_CadastroLoja
+    {
+        public static int qntMercadoria;
+        public static double valorMercadoria, valorTotal,mediaValores;
+        public static void Main(string[]args)
+        {
+            /*Uma loja está levantando o valor total de todas as mercadorias em estoque. Escreva um algoritmo que 
+              permita a entrada das seguintes informações: 
+              
+            a) o número total de mercadorias no estoque; 
+            b) o valor de cada mercadoria.
+              Ao final imprimir o valor total em estoque e a média de valor das mercadorias.  
+            */
+
+            Console.Write("Digite o total de mercadorias no estoque: ");
+            qntMercadoria = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= qntMercadoria; i++)
+            {
+                Console.Write("Digite o valor do produto {0}: ",i);
+                valorMercadoria = Convert.ToDouble(Console.ReadLine());
+                if (valorMercadoria<0)
+                {
+                    Console.WriteLine("\n");
+                    Console.WriteLine("NÃO EXISTE MERCADORIA COM VALOR NEGATIVO");
+                    break;
+                }
+                valorTotal = valorTotal + valorMercadoria;
+            }
+            mediaValores = valorTotal / qntMercadoria;
+            Console.WriteLine("\n");
+            Console.WriteLine("============== RESULTADO ============\n");              
+            Console.WriteLine("O VALOR MEDIO É: R${0}",mediaValores.ToString("##.##"));
+            Console.WriteLine("VALOR TOTAL : R${0}", valorTotal);
+            Console.WriteLine("ESTOQUE: {0} produtos", qntMercadoria);
+            Console.ReadKey();
+        }
+    }
+}
