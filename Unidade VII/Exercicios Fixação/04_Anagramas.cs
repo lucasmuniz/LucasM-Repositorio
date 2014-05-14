1be1bc10b7c95b9cf714275f0a08cf56382249bf
Lucas Muniz <lucas_goedert@hotmail.com>
2014-05-14 09:03:03 -0300

    exercicios

--- Unidade VII/Exercicios Fixação/04_Anagramas.cs
+++ Unidade VII/Exercicios Fixação/04_Anagramas.cs
@@ -6,6 +6,36 @@ class _04_Anagrams
 {
     public static void Main04(string[]args)
     {
+        string biro = "biro";
+
+        Console.Write("{0}{1}{2}{3} ", biro[0].ToString(), biro[1].ToString(), biro[2].ToString(), biro[3].ToString()); //biro
+        Console.Write("{0}{1}{2}{3} ", biro[0].ToString(), biro[1].ToString(), biro[3].ToString(), biro[2].ToString()); //bior
+        Console.Write("{0}{1}{2}{3} ", biro[0].ToString(), biro[2].ToString(), biro[1].ToString(), biro[3].ToString()); //brio
+        Console.Write("{0}{1}{2}{3} ", biro[0].ToString(), biro[2].ToString(), biro[3].ToString(), biro[1].ToString()); //broi
+        Console.Write("{0}{1}{2}{3} ", biro[0].ToString(), biro[3].ToString(), biro[1].ToString(), biro[2].ToString()); //boir
+        Console.WriteLine("{0}{1}{2}{3}", biro[0].ToString(), biro[3].ToString(), biro[2].ToString(), biro[1].ToString()); //bori
+
+        Console.Write("{0}{1}{2}{3} ", biro[1].ToString(), biro[0].ToString(), biro[2].ToString(), biro[3].ToString()); //ibro
+        Console.Write("{0}{1}{2}{3} ", biro[1].ToString(), biro[0].ToString(), biro[3].ToString(), biro[2].ToString()); //ibor
+        Console.Write("{0}{1}{2}{3} ", biro[1].ToString(), biro[2].ToString(), biro[0].ToString(), biro[3].ToString()); //irbo
+        Console.Write("{0}{1}{2}{3} ", biro[1].ToString(), biro[2].ToString(), biro[3].ToString(), biro[0].ToString()); //irob
+        Console.Write("{0}{1}{2}{3} ", biro[1].ToString(), biro[3].ToString(), biro[0].ToString(), biro[2].ToString()); //iobr
+        Console.WriteLine("{0}{1}{2}{3}", biro[1].ToString(), biro[3].ToString(), biro[2].ToString(), biro[0].ToString()); //iorb
+
+        Console.Write("{0}{1}{2}{3} ", biro[2].ToString(), biro[0].ToString(), biro[1].ToString(), biro[3].ToString()); //rbio
+        Console.Write("{0}{1}{2}{3} ", biro[2].ToString(), biro[0].ToString(), biro[3].ToString(), biro[1].ToString()); //rboi
+        Console.Write("{0}{1}{2}{3} ", biro[2].ToString(), biro[1].ToString(), biro[0].ToString(), biro[3].ToString()); //ribo
+        Console.Write("{0}{1}{2}{3} ", biro[2].ToString(), biro[1].ToString(), biro[3].ToString(), biro[0].ToString()); //riob
+        Console.Write("{0}{1}{2}{3} ", biro[2].ToString(), biro[3].ToString(), biro[1].ToString(), biro[2].ToString()); //roib
+        Console.WriteLine("{0}{1}{2}{3}", biro[2].ToString(), biro[3].ToString(), biro[0].ToString(), biro[1].ToString()); //robi
+
+        Console.Write("{0}{1}{2}{3} ", biro[3].ToString(), biro[0].ToString(), biro[1].ToString(), biro[2].ToString()); //obir
+        Console.Write("{0}{1}{2}{3} ", biro[3].ToString(), biro[0].ToString(), biro[1].ToString(), biro[1].ToString()); //obri
+        Console.Write("{0}{1}{2}{3} ", biro[3].ToString(), biro[1].ToString(), biro[0].ToString(), biro[2].ToString()); //oibr
+        Console.Write("{0}{1}{2}{3} ", biro[3].ToString(), biro[1].ToString(), biro[2].ToString(), biro[0].ToString()); //oirb
+        Console.Write("{0}{1}{2}{3} ", biro[3].ToString(), biro[2].ToString(), biro[0].ToString(), biro[1].ToString()); //orbi
+        Console.Write("{0}{1}{2}{3}", biro[3].ToString(), biro[2].ToString(), biro[1].ToString(), biro[0].ToString()); //orib
+        Console.ReadKey();
        
     }
 }
