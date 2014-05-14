b1684d4353a2f55649258141c78de17384b6dd93
Lucas Muniz <lucas_goedert@hotmail.com>
2014-05-14 09:16:14 -0300

    algumas alteraciones

---
+++ Unidade VII/Exercicios Fixação/06_LançamentoDados.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unidade_VII.Exercicios_Fixação
+{
+    class _06_LançamentoDados
+    {
+        public static void Main(string[] args)
+        {
+            /*Faça um programa que simule um lançamento de dados. Lance o dado 100 vezes e armazene os resultados em um vetor.
+             * Depois, mostre quantas vezes cada valor foi conseguido. Dica: use um vetor de contadores(1-6) e 
+             * uma função para gerar números aleatórios, simulando os lançamentos dos dados.
+            */
+
+        }
+    }
+}
