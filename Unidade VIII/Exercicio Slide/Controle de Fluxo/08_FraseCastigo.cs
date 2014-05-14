57ecd4b260319d16d4084ceda9d8e66292213483
Lucas Muniz <lucas_goedert@hotmail.com>
2014-05-14 11:49:34 -0300

    exercicios complemetares

--- Unidade VIII/Exercicio Slide/Controle de Fluxo/08_FraseCastigo.cs
+++ Unidade VIII/Exercicio Slide/Controle de Fluxo/08_FraseCastigo.cs
@@ -8,11 +8,11 @@ namespace Unidade_VIII.Exercicio_Slide.Controle_de_Fluxo
 {
     class _08_FraseCastigo
     {
-        public static void Main(string[] args)
+        public static void Main08(string[] args)
         {
             while (true) //LOOP INFINITO
             {
-                Console.WriteLine("Eu não andarei de skate nos corredores");                          
+                Console.WriteLine("Eu não andarei de skate nos corredores");                    
             }
         }
     }
---
+++ Unidade VIII/Exercicios Complementares/01_NumerosAleatorios.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Unidade_VIII.Exercicios_Complementares
+{
+    internal class _01_NumerosAleatorios
+    {
+        public static void Main(string[] args)
+        {
+            /* Crie um programa em C# que gere dois números aleatórios e exiba o valor desses números.
+               Além disso, esse programa deve exibir a mensagem “Primeiro > Segundo” se o primeiro número for
+               maior do que o segundo, a mensagem “Segundo > Primeiro” se o segundo número for maior do que o primeiro e
+               mensagem “Primeiro = Segundo” se o primeiro número for igual ao segundo.
+           */
+
+            Random geradorNum = new Random();
+
+            int primeiro;
+            int segundo;
+
+            primeiro = geradorNum.Next(0, 10);
+            segundo = geradorNum.Next(0, 10);
+            if (primeiro > segundo)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Primeiro: {0} Segundo {1}", primeiro, segundo);
+                Console.WriteLine("PRIMEIRO > SEGUNDO");
+            }
+            if (primeiro < segundo)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Primeiro: {0} Segundo {1}", primeiro, segundo);
+                Console.WriteLine("PRIMEIRO < SEGUNDO");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
