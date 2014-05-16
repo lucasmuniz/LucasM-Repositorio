12bb38204e0e0df9abd5b302043b2fbfdd309dd4
lucasmuniz <lucas_goedert@hotmail.com>
2014-05-16 01:54:21 -0300

    Exercicio Faltante adicionado

--- Unidade VII/Exercicios Fixação/06_LançamentoDados.cs
+++ Unidade VII/Exercicios Fixação/06_LançamentoDados.cs
@@ -1,4 +1,6 @@
-﻿namespace Unidade_VII.Exercicios_Fixação
+﻿using System;
+
+namespace Unidade_VII.Exercicios_Fixação
 {
     internal class _06_LançamentoDados
     {
@@ -8,6 +10,19 @@
              * Depois, mostre quantas vezes cada valor foi conseguido. Dica: use um vetor de contadores(1-6) e
              * uma função para gerar números aleatórios, simulando os lançamentos dos dados.
             */
+
+            int[] contador = new int[6];
+            Random gerador = new Random();
+            for (int i = 0; i < 100; i++)
+            {
+                contador[gerador.Next(0, 6)]++;
+            }
+            Console.WriteLine("=== RESULTADO DOS LANÇAMENTOS ===");
+            for (int i = 0; i < contador.Length; i++)
+            {
+                Console.WriteLine("Numero {0}: {1} vezes", i + 1, contador[i]);
+            }
+            Console.ReadKey();
         }
     }
 }
