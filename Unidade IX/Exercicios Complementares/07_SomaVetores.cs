6b4d6f3fe5d0d8beb4e937cecd426e6c30e94ad7
Lucas Muniz <lucas_goedert@hotmail.com>
2014-05-20 11:16:27 -0300

    Exercicios!

---
+++ Unidade IX/Exercicios Complementares/07_SomaVetores.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Unidade_IX.Exercicios_Complementares
+{
+    internal class _07_SomaVetores
+    {
+        public static int somaSubtração;
+
+        public static void Main07(string[] args)
+        {
+            /*Faça um algoritmo que leia dois vetores (A e B) de 50 posições de números inteiros. O algoritmo deve, então,
+             * subtrair o primeiro elemento de A do último de B, acumulando o valor, subtrair o segundo elemento de A do
+             * penúltimo de B, acumulando o valor, e assim por diante. Mostre o resultado da soma final.*/
+            int[] vetorA = new int[50];
+            int[] vetorB = new int[50];
+            int subtraçaõ;
+
+            for (int i = 0; i < vetorA.Length; i++)
+            {
+                Console.Write("Digite o valor da posição {0} do vetor A: ", i);
+                vetorA[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (int i = 0; i < vetorB.Length; i++)
+            {
+                Console.Write("Digite o valor da posição {0} do vetor B: ", i);
+                vetorB[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (int i = 0; i < 50; i++)
+            {
+                subtraçaõ = vetorA[i] - vetorB[50 - 1];
+                somaSubtração += subtraçaõ;
+            }
+            Console.WriteLine("");
+            Console.WriteLine("========== RESULTADO ==========\n");
+
+            Console.WriteLine("Soma de todas as subtrações feitas: {0}",somaSubtração);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Unidade IX/Exercicios Complementares/08_NumerosLoteria.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unidade_IX.Exercicios_Complementares
+{
+    class _08_NumerosLoteria
+    {
+        public static void Main(string[] args)
+        {
+            /*Escreva um algoritmo que leia um vetor de 13 elementos inteiros, que é o Gabarito de um teste da loteria
+             * esportiva, contendo os valores 1(coluna 1), 2 (coluna 2) e 3 (coluna do meio). Leia, a seguir, para cada
+             * apostador, o número do seu cartão e um vetor de Respostas de 13 posições. Verifique para cada apostador os
+             * números de acertos, comparando o vetor de Gabarito com o vetor de Respostas. Escreva o número do apostador e 
+             * o número de acertos. Se o apostador tiver 13 acertos, mostrar a mensagem "Ganhador".*/
+
+
+        }
+    }
+}
