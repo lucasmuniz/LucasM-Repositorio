93ee49f3145169ceaae30d89fa7ba7f7833e7c2d
Lucas Muniz <lucas_goedert@hotmail.com>
2014-05-21 08:35:56 -0300

    CODEMAID

--- Unidade IX/Exercicios Complementares/04_TrocaValoresVetor.cs
+++ Unidade IX/Exercicios Complementares/04_TrocaValoresVetor.cs
@@ -1,33 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Unidade_IX.Exercicios_Complementares
 {
-    class _04_TrocaValoresVetor
+    internal class _04_TrocaValoresVetor
     {
         public static void Main04(string[] args)
         {
-          /* Escreva um algoritmo que leia um vetor inteiro de 12 posições e crie um segundo vetor, substituindo
-             os valores nulos por 1. Mostre os 2 vetores.*/
+            /* Escreva um algoritmo que leia um vetor inteiro de 12 posições e crie um segundo vetor, substituindo
+               os valores nulos por 1. Mostre os 2 vetores.*/
             int[] vetor = new int[12];
             int[] vetorFormatado = new int[12];
             for (int i = 0; i < vetor.Length; i++)
             {
-                Console.Write("Digite o valor para a posicao {0}: ",i);
+                Console.Write("Digite o valor para a posicao {0}: ", i);
                 vetor[i] = Convert.ToInt32(Console.ReadLine());
                 vetorFormatado[i] = vetor[i];
-                if (vetor[i]==0)
+                if (vetor[i] == 0)
                 {
                     vetorFormatado[i] = 1;
                 }
-                
             }
             Console.WriteLine("");
             Console.WriteLine("========= RESULTADO =========\n");
-            
+
             Console.WriteLine("Vetor Original:\n");
             for (int i = 0; i < vetor.Length; i++)
             {
@@ -37,7 +32,7 @@ namespace Unidade_IX.Exercicios_Complementares
             Console.WriteLine("Vetor Formatado:\n");
             for (int i = 0; i < vetorFormatado.Length; i++)
             {
-                Console.WriteLine("Valor da posição [{0}]: {1}",i,vetorFormatado[i]);
+                Console.WriteLine("Valor da posição [{0}]: {1}", i, vetorFormatado[i]);
             }
             Console.ReadKey();
         }
--- Unidade IX/Exercicios Complementares/06_Locadora.cs
+++ Unidade IX/Exercicios Complementares/06_Locadora.cs
@@ -1,35 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Unidade_IX.Exercicios_Complementares
 {
-    class _06_Locadora
+    internal class _06_Locadora
     {
         public static void Main06(string[] args)
         {
-            /*Uma locadora de vídeos tem guardada, em um vetor de 50 posições, a quantidade de filmes retirados por seus 
-             * clientes durante o ano de 2004. Agora, está locadora está fazendo uma promoção e, para cada 10 filmes retirados, 
-             * o cliente tem direito a uma locação grátis. Faça um algoritmo que crie um outro vetor contendo a quantidade de 
+            /*Uma locadora de vídeos tem guardada, em um vetor de 50 posições, a quantidade de filmes retirados por seus
+             * clientes durante o ano de 2004. Agora, está locadora está fazendo uma promoção e, para cada 10 filmes retirados,
+             * o cliente tem direito a uma locação grátis. Faça um algoritmo que crie um outro vetor contendo a quantidade de
              * locações gratuitas a que cada cliente tem direito.*/
             int[] totFilmes = new int[50];
             int[] filmesGratis = new int[50];
 
             for (int i = 0; i < totFilmes.Length; i++)
             {
-                Console.WriteLine("Quantos filmes o cliente numero {0} alugou? ",i+1);
+                Console.WriteLine("Quantos filmes o cliente numero {0} alugou? ", i + 1);
                 totFilmes[i] = Convert.ToInt32(Console.ReadLine());
                 filmesGratis[i] = totFilmes[i] / 10;
             }
             Console.WriteLine("======== RESULTADO =======");
             for (int i = 0; i < filmesGratis.Length; i++)
-			{
-			 Console.WriteLine("O cliente numero {0} tem direito à {1} filmes grátis!",i,filmesGratis[i]);
-			}
+            {
+                Console.WriteLine("O cliente numero {0} tem direito à {1} filmes grátis!", i, filmesGratis[i]);
+            }
             Console.ReadKey();
-            
         }
     }
 }
