0526184eddffa87c9cbb4ddc731f4871968e287d
lucasmuniz <lucas_goedert@hotmail.com>
2014-05-23 01:27:48 -0300

    Fixação

---
+++ Unidade X/Exercicios Fixação/01_ConsumoCombustivel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Unidade_X.Exercicios_Fixação
+{
+    internal class _01_ConsumoCombustivel
+    {
+        public static double KmPercorridos, LitrosAbastecidos, Calculo;
+
+        public static void Main01(string[] args)
+        {
+            //Na pasta funcoes-ou-metodos, crie um arquivo chamado ConsumoDeCombustivel.cs. Implemente um programa em
+            //C# definindo um método que realize o cálculo do consumo de combustível de um veículo.
+
+            Console.Write("Digite quantos km você percorreu: ");
+            KmPercorridos = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Digite quantos litros abasteceu: ");
+            LitrosAbastecidos = Convert.ToDouble(Console.ReadLine());
+            Consumo();
+            Console.WriteLine("Seu consumo foi de : {0} km/l.", Calculo.ToString("##.##"));
+            Console.ReadKey();
+        }
+
+        public static void Consumo()
+        {
+            Calculo = KmPercorridos / LitrosAbastecidos;
+        }
+    }
+}
---
+++ Unidade X/Exercicios Fixação/02_JuroComposto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Unidade_X.Exercicios_Fixação
+{
+    internal class _02_JuroComposto
+    {
+        public static double CapitalInicial, Montante, TaxaDeJuros, Tempo, calculo1;
+
+        public static void Main02(string[] args)
+        {
+            /*Na pasta funcoes-ou-metodos, crie um arquivo chamado JurosComposto.cs.
+              Implemente um programa em C# definindo um método que realize o cálculo do montante obtido com a
+              aplicação de juros compostos. M = C * (1 + i)t */
+
+            Console.Write("Digite o seu Capital inicial: ");
+            CapitalInicial = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Digite a taxa de juros: ");
+            TaxaDeJuros = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Digite o tempo: ");
+            Calculos();
+            Console.WriteLine("============= RESULTADO ===========\a\n");
+            Console.WriteLine("Montante: {0}", Montante.ToString("##.##"));
+            Console.ReadKey();
+        }
+
+        public static void Calculos()
+        {
+            Tempo = Convert.ToDouble(Console.ReadLine());
+            calculo1 = Math.Pow(1 + TaxaDeJuros, Tempo);
+            Montante = CapitalInicial * calculo1;
+            //Montante=CapitalInicial*(1+TaxaDeJuros);
+        }
+    }
+}
