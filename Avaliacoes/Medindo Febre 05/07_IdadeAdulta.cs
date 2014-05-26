8bdbdc1d93cadae5b9c048d4f013102a7a8593ab
Lucas Muniz <lucas_goedert@hotmail.com>
2014-05-26 11:11:41 -0300

    Alterações

--- Avaliacoes/Medindo Febre 05/07_IdadeAdulta.cs
+++ Avaliacoes/Medindo Febre 05/07_IdadeAdulta.cs
@@ -1,23 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Avaliacoes.Medindo_a_Febre_05
 {
-    class _07_IdadeAdulta
+    internal class _07_IdadeAdulta
     {
         public static void Main07(string[] args)
         {
             int idade;
             Console.Write("Digite a idade: ");
             idade = Convert.ToInt16(Console.ReadLine());
-            if (idade<0)
+            if (idade < 0)
             {
                 Console.WriteLine("IDADE INVALIDA!");
             }
-            else if (idade==0)
+            else if (idade == 0)
             {
                 Console.WriteLine("VOCE NAO PODE TER IDADE IGUAL A ZERO!");
             }
@@ -25,7 +21,7 @@ namespace Avaliacoes.Medindo_a_Febre_05
             Console.WriteLine("======== RESULTADO ========");
             string resultado = idade >= 21 ? "Adulta" : "Não é adulta";
             Console.WriteLine(resultado);
-            Console.WriteLine("E sua idade é: {0} Anos",idade);
+            Console.WriteLine("E sua idade é: {0} Anos", idade);
 
             Console.ReadKey();
         }
--- Avaliacoes/Medindo Febre 05/08_GerenteBanco.cs
+++ Avaliacoes/Medindo Febre 05/08_GerenteBanco.cs
@@ -1,17 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Avaliacoes.Medindo_a_Febre_05
 {
-    class _08_GerenteBanco
+    internal class _08_GerenteBanco
     {
-        public static int Hora,ContPessoas;
-        public static void Main08(string[]args)
+        public static int Hora, ContPessoas;
+
+        public static void Main08(string[] args)
         {
-           
             for (int i = 0; i <= 9; i++)
             {
                 Console.WriteLine("Digite o horário que você entrou no banco: USE FORMATO 24HRS ");
@@ -24,13 +20,12 @@ namespace Avaliacoes.Medindo_a_Febre_05
                 {
                     Console.WriteLine("HORA INVALIDA");
                     break;
-                }           
+                }
             }
             Console.WriteLine("\n");
-            Console.WriteLine("O total que entraram no banco no intervalo de horas foi: {0} pessoas.",ContPessoas);
+            Console.WriteLine("O total que entraram no banco no intervalo de horas foi: {0} pessoas.", ContPessoas);
 
             Console.ReadKey();
-
         }
     }
 }
