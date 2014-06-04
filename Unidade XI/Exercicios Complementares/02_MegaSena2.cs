6fca585d3edc0bf407a56be26ffaad45628de16c
Lucas Muniz <lucas_goedert@hotmail.com>
2014-06-04 11:30:21 -0300

    Alterações

--- Unidade XI/Exercicios Complementares/02_MegaSena2.cs
+++ Unidade XI/Exercicios Complementares/02_MegaSena2.cs
@@ -1,31 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Unidade_XI.Exercicios_Complementares
 {
-    
-    class _02_MegaSena2
+    internal class _02_MegaSena2
     {
         /*2) Continuando na questão 1, o usuário agora deve informar os 6 números que ele deseja escolher. Esses 6 dados devem ser
-         * armazenados em um array também, como os números premiados da questão 1. Caso o usuário escolha um número fora do intervalo 
+         * armazenados em um array também, como os números premiados da questão 1. Caso o usuário escolha um número fora do intervalo
          * do cartão da mega-sena, o software deve avisa-lo.*/
 
         public static int[] arrayMega = new int[6];
         public static Random gerador = new Random();
         public static int[] gabaritoMega = new int[6];
+
         public static void Main02(string[] args)
         {
-
             GabaritoMega();
 
             for (int i = 0; i < arrayMega.Length; i++)
             {
                 Console.WriteLine("Digite o numero que deseja marcar: ");
                 arrayMega[i] = Convert.ToInt16(Console.ReadLine());
-                if (arrayMega[i]<0||arrayMega[i]>60)
+                if (arrayMega[i] < 0 || arrayMega[i] > 60)
                 {
                     Console.WriteLine("Voce Digitou errado!!!");
                     return;
@@ -34,8 +29,6 @@ namespace Unidade_XI.Exercicios_Complementares
 
             ResultadoMega();
             Console.ReadKey();
-
-
         }
 
         private static void ResultadoMega()
--- Unidade XI/Exercicios Complementares/03_MegaSena3.cs
+++ Unidade XI/Exercicios Complementares/03_MegaSena3.cs
@@ -8,13 +8,55 @@ namespace Unidade_XI.Exercicios_Complementares
 {
     class _03_MegaSena3
     {
-        public static void Main(string[]args)
+        public static int[] arrayMega = new int[6];
+        public static Random gerador = new Random();
+        public static int[] gabaritoMega = new int[6];
+        public static void Main(string[] args)
         {
 
             /*3) Continuando no software da mega-sena. Faça um método que Ordene do menor para o maior número. E mostre os números 
                  sorteados da seguinte forma: [ 2 ] [34] [36] [45] [51] [59]*/
-            
 
+            GabaritoMega();
+
+            for (int i = 0; i < arrayMega.Length; i++)
+            {
+                Console.WriteLine("Digite o numero que deseja marcar: ");
+                arrayMega[i] = Convert.ToInt16(Console.ReadLine());
+                if (arrayMega[i] < 0 || arrayMega[i] > 60)
+                {
+                    Console.WriteLine("Voce Digitou errado!!!");
+                    return;
+                }
+            }
+
+            ResultadoMega();
+            Console.ReadKey();
         }
+
+        private static void ResultadoMega()
+        {
+            Console.WriteLine("APOSTOU:");
+            for (int i = 0; i < arrayMega.Length; i++)
+            {
+                Console.WriteLine(arrayMega[i]);
+            }
+            Console.WriteLine("");
+            Console.WriteLine("GABARITO:");
+            for (int i = 0; i < gabaritoMega.Length; i++)
+            {
+                Console.WriteLine(gabaritoMega[i]);
+            }
+        }
+
+        private static void GabaritoMega()
+        {
+            for (int i = 0; i < gabaritoMega.Length; i++)
+            {
+                gabaritoMega[i] = gerador.Next(0, 60);
+            }
+        }
+
     }
+    
 }
