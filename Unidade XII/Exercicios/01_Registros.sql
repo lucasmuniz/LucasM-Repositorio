78255866d95dd88cf7a18f163a3cdec4dc4ae485
Lucas Muniz <lucas_goedert@hotmail.com>
2014-06-10 09:12:42 -0300

    Adicionado questoes

---
+++ Unidade XII/Exercicios/01_Registros.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Aluno] (
+    [Id]       INT           IDENTITY (1, 1) NOT NULL,
+    [Nome]     VARCHAR (50)  NULL,
+    [Telefone] INT           NULL,
+    [Altura]   FLOAT (53)    NULL,
+    [Aprovado] NVARCHAR (50) NULL,
+    [Email]    NVARCHAR (50) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    UNIQUE NONCLUSTERED ([Nome] ASC)
+);
