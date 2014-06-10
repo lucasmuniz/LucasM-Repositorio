8d1c40e34cff49e73af224b6980f0ff3a0334dc7
Lucas Muniz <lucas_goedert@hotmail.com>
2014-06-10 09:33:56 -0300

    Exercicios

---
+++ Unidade XII/Exercicios/02_Registros.sql
@@ -0,0 +1,5 @@
+﻿SET IDENTITY_INSERT [dbo].[Aluno] ON
+INSERT INTO [dbo].[Aluno] ([Id], [Nome], [Telefone], [Altura], [Aprovado], [Email]) VALUES (1, N'Lucas', 99520610, 1.9, N'Aprovado', N'lucas_goedert@hotmail.com')
+INSERT INTO [dbo].[Aluno] ([Id], [Nome], [Telefone], [Altura], [Aprovado], [Email]) VALUES (2, N'Giorgi', 99566459, 1.95, N'Reprovado', N'giorgi@hotmail.com')
+INSERT INTO [dbo].[Aluno] ([Id], [Nome], [Telefone], [Altura], [Aprovado], [Email]) VALUES (3, N'Camila', 88555554, 1.8, N'Reprovado', N'camila@hotmail.com')
+SET IDENTITY_INSERT [dbo].[Aluno] OFF
