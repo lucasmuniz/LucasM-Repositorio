42c0bc22fdc9c4a95a38b222c9415cd2a784ac36
lucasmuniz <lucas_goedert@hotmail.com>
2014-06-19 15:46:58 -0300

    ExerciciosSQL

--- Unidade XII/Trabalho/08_Medicina.sql
+++ Unidade XII/Trabalho/08_Medicina.sql
@@ -1,3 +1,3 @@
 ﻿ --8)	Faça um comando SQL que retorne o nome de todas as mulheres matriculadas no curso de Medicina. 
- Select a.Nome AS Nome, c.Nome as Curso from tbMatricula as mat INNER JOIN tbCurso AS c on c.id = mat.Curso_Id INNER JOIN
-  tbAluno as a ON a.Id = mat.Aluno_Id where c.Nome = 'Medicina%' AND a.Sexo = 'F';
+ Select * from tbMatricula AS mat inner join tbCurso AS c ON c.Id = mat.Curso_Id inner join tbAluno as a ON a.Id = mat.Aluno_Id WHERE 
+ mat.Curso_Id = 1 and a.Sexo = 'F';
