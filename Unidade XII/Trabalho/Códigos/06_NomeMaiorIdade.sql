62388a844b5e3b675750a17af157a7c9388e3853
Lucas Muniz <lucas_goedert@hotmail.com>
2014-06-24 11:43:17 -0300

    Mudnaças

--- Unidade XII/Trabalho/Códigos/06_NomeMaiorIdade.sql
+++ Unidade XII/Trabalho/Códigos/06_NomeMaiorIdade.sql
@@ -1,3 +1,3 @@
 ﻿-- 6)	Desenvolva um comando SQL que retorne o nome de todos os alunos maiores que 18 anos. 
  
-SELECT Nome from TBAluno where DataNascimento>'01/01/1996'
+SELECT Nome from TBAluno where DataNascimento<'01/01/1996'
