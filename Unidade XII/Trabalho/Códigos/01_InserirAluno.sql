13f3af05f28d4568841bcaecd96bbf60e252a152
Lucas Muniz <lucas_goedert@hotmail.com>
2014-06-24 10:51:44 -0300

    Mudanças

--- Unidade XII/Trabalho/Códigos/01_InserirAluno.sql
+++ Unidade XII/Trabalho/Códigos/01_InserirAluno.sql
@@ -1,4 +1,4 @@
 ﻿-- 1) Faça um comando SQL para matricular o aluno “Pedro César” no curso de Informática. Os dados devem ser inseridos na tabela TBMATRÍCULA. 
 
---INSERT INTO TBAluno(Nome,DataNascimento,Email,Sexo) values('Pedro Cezar', '1996-06-16', 'pedro@hotmail.com', 'M')
---INSERT INTO TBMATRICULA(Curso_Id,Aluno_Id) VALUES (4,7)
+INSERT INTO TBAluno(Nome,DataNascimento,Email,Sexo) values('Pedro Cezar', '1996-06-16', 'pedro@hotmail.com', 'M')
+INSERT INTO TBMATRICULA(Curso_Id,Aluno_Id) VALUES (4,7)
