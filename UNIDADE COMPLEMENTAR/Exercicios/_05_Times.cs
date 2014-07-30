d8f21b339dbdb5d9c679c74d335a572eb3731850
LucasMuniz <lucas_goedert@hotmail.com>
2014-07-30 11:28:41 -0300

    mudançças

--- UNIDADE COMPLEMENTAR/Exercicios/_05_Times.cs
+++ UNIDADE COMPLEMENTAR/Exercicios/_05_Times.cs
@@ -1,29 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace UNIDADE_COMPLEMENTAR.Exercicios
 {
-    class _05_Times
+    internal class _05_Times
     {
         public static void Main(string[] args)
         {
-            /*5) Crie um algoritmo com uma coleção com todos os times da copa. Sabe-se que são 32 times que serão sorteados e 
+            /*5) Crie um algoritmo com uma coleção com todos os times da copa. Sabe-se que são 32 times que serão sorteados e
              * divididos em 8 grupos, do A ao H. Depois de sorteados e alocados o algoritmo deve imprimir na tela todos os grupos.*/
             Dictionary<string, char> _dictionary = new Dictionary<string, char>();
-            List<char> Grupos = new List<char>() { 'A', 'B', 'C','D','E','F','G','H'};
-            List<string> Times = new List<string>() { "Alemanha", "Argentina", "Argélia","Brasil"};
+            List<char> Grupos = new List<char>() { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H' };
+            List<string> Times = new List<string>() { "Alemanha", "Argentina", "Argélia", "Brasil" };
             Random sort = new Random();
 
             for (int i = 0; i < 4; i++)
-            {      
+            {
                 int time = sort.Next(0, Times.Count);
                 _dictionary.Add(Times[time], Grupos[0]);
                 Times.Remove(Times[time]);
             }
-
         }
     }
 }
